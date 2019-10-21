e45e0d22be74977af1fe08c70fa96fb723cddcde
fredlabbe <labbef.hockey@gmail.com>
2019-10-21 19:37:45 -0400

    Ex5: added another predator that takes in W A S D

--- Exercises/exercise5/js/script.js
+++ Exercises/exercise5/js/script.js
@@ -13,6 +13,7 @@ author, and this description to match your project!
 
 // This will contain our Predator object
 let tiger;
+let lion;
 let antelope;
 let warthog;
 
@@ -32,7 +33,8 @@ function preload() {
 
 function setup() {
   createCanvas(1280, 720);
-  tiger = new Predator(width/2, height/2, 5, color(255, 165, 0), 40);
+  tiger = new Predator(width/2, height/2, 5, color(255, 165, 0), 40, 38, 40, 37, 39);
+  lion = new Predator(random(0,width),random(0,height), 5, color(255, 10, 10), 40, 87, 83, 65, 68);
   antelope = new Prey(random(0,width),random(0,height),8, color(143, 111, 71), 15);
   warthog = new Prey(random(0,width),random(0,height),8, color(204, 168, 151), 10);
 
@@ -46,18 +48,29 @@ function setup() {
 function draw() {
   background(0);
 
+  //they take the users' inputs in
   tiger.handleInput();
+  lion.handleInput();
+
+  //Making the animals move
   tiger.move();
+  lion.move();
   antelope.move();
   warthog.move();
 
+  //Displaying the animals on the screen
   tiger.display();
+  lion.display();
   antelope.display();
   warthog.display();
+
+  //calls the functions that checks and handles if the preys are eaten
   tiger.handleEating(antelope);
   tiger.handleEating(warthog);
+  lion.handleEating(antelope);
+  lion.handleEating(warthog);
 
-  if(tiger.health<0){
+  if(tiger.health<0 ){
 
   }
 
