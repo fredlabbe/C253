026b625796f6b3f76fb1f85b441ca8347897211b
fredlabbe <labbef.hockey@gmail.com>
2019-10-30 19:18:12 -0400

    Ex6: Forgot ot put a number between the two ","

--- Exercises/Exercise6/js/script.js
+++ Exercises/Exercise6/js/script.js
@@ -23,9 +23,11 @@ let bee;
 //
 // Sets up a canvas
 // Creates objects for the predator and three prey
-function setup(){ 
+///////FIXED
+function setup(){
   createCanvas(windowWidth, windowHeight);
-  tiger = new Predator(100, , 100, 5, color(200, 200, 0), 40);
+  //////Fixed 
+  tiger = new Predator(100, 100, 100, 5, color(200, 200, 0), 40);
   antelope = new Prey(100, 100, 10, color(255, 100, 10), 50);
   zebra = new Prey(100, 8, color(255, 255, 255), 60);
   bee = new Prey(100, 100, 20, color(255, 255, 0), 10);
